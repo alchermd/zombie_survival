1a543852f99ab84102ec41a5f278234b14e50b41
alchermd <johnalcherdoloiras@gmail.com>
2017-10-12 23:55:36 +0800

    Create a BaseSprite class in which the Player and Zombie class inherits from.

--- gamelib/sprite.py
+++ gamelib/sprite.py
@@ -5,35 +5,35 @@ import pygame
 import gamelib.palette as p
 
 
-class Player(pygame.sprite.Sprite):
+class BaseSprite(pygame.sprite.Sprite):
     """
-    Represents the player controlled sprite.
+    Represents the base sprite that moves laterally.
     """
     def __init__(self, color: pygame.Color, width: int, height: int):
         """
-        Creates a new Player instance.
+        Creates a new BaseSprite instance.
 
         Args:
-            color: a 3 value tuple that represents the Player's RGB color.
-            width: the width of the Player.
-            height: the height of the Player.
+            color: a 3 value tuple that represents the BaseSprite's RGB color.
+            width: the width of the BaseSprite.
+            height: the height of the BaseSprite.
         """
         super().__init__()
 
-        # Create the Player's surface.
+        # Create the BaseSprite's surface.
         self.image = pygame.Surface((width, height))
         self.image.fill(color)
         
         # Save the dimensions to the rect attribute.
         self.rect = self.image.get_rect()
 
-        # Player velocity.
+        # BaseSprite velocity.
         self.x_speed = 0
 
 
     def set_position(self, x: int, y: int):
         """
-        Sets the Player's position on the screen.
+        Sets the BaseSprite's position on the screen.
 
         Args:
             x: the x-coordinate.
@@ -44,10 +44,10 @@ class Player(pygame.sprite.Sprite):
 
     def move_laterally(self, x_speed: int):
         """
-        Sets the Player's x_speed.
+        Sets the BaseSprite's x_speed.
 
         Args:
-            x_speed: the new velocity in which the Player moves on
+            x_speed: the new velocity in which the BaseSprite moves on
                 the x-axis.
         """
         self.x_speed = x_speed
@@ -58,5 +58,19 @@ class Player(pygame.sprite.Sprite):
         General purpose update method that gets ran when a containing
         pygame.sprite.Group run its own update method.
         """
-        # Move the Player on the x-axis.
+        # Move the BaseSprite on the x-axis.
         self.rect.x += self.x_speed
+
+
+class Player(BaseSprite):
+    """
+    Represents the player-controlled sprite.
+    """
+    pass
+
+
+class Zombie(BaseSprite):
+    """
+    Represents the zombie sprite that tries to overwhelm the player.
+    """
+    pass
