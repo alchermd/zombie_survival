0396bf843687ecb747eaa4dcbc2384959efd9056
alchermd <johnalcherdoloiras@gmail.com>
2017-10-13 22:06:33 +0800

    Extend the Player class to generate and handle bullets.  A placeholder Bullet class is also created.

--- gamelib/sprite.py
+++ gamelib/sprite.py
@@ -62,11 +62,72 @@ class BaseSprite(pygame.sprite.Sprite):
         self.rect.x += self.x_speed
 
 
+class Bullet(BaseSprite):
+    """
+    Represents the bullet that the Player fires to try and fend off
+    the hordes of zombies.
+    """
+    pass
+
+
 class Player(BaseSprite):
     """
     Represents the player-controlled sprite.
     """
-    pass
+    def __init__(self, color: pygame.Color, width: int, height: int, bullet_speed: int):
+        """
+        Creates a new Player instance. See the BaseSprite class constructor
+        for more information.
+        
+        Args:
+            bullet_x_speed: the speed in which the player's bullets travel at
+                the x-axis.
+        """
+        super().__init__(color, width, height)
+
+        # Bullets from the player.
+        self.bullets = pygame.sprite.Group()
+
+        # Set an initial bullet velocity.
+        self.bullet_speed = bullet_speed
+        self.bullet_x_speed = bullet_speed
+
+
+    def calc_bullet_info(self, bullet: Bullet) -> list:
+        """
+        Calcuates the position and velocity of the player's bullet.
+        Returns -> [x, y, x_speed]
+            x: x-coordinate of the bullet
+            y: y-coordinate of the bullet
+            x_speed: velocity of the bullet on the x-axis
+        """
+        x = None
+        # Place the bullet at the right of the player if
+        # the player is moving right.
+        if self.bullet_x_speed > 0:
+            x = self.rect.right + 10  # Some padding.
+        
+        # And vice-versa if the player is moving left.
+        elif self.bullet_x_speed < 0:
+            x = self.rect.left - 10   # Some padding.
+
+        # The bullet is half the player's height.
+        y = self.rect.center[1] - bullet.image.get_height() / 2
+
+        return [x, y, self.bullet_x_speed]
+
+
+    def update(self):
+        """
+        Modified update method to update the bullet's velocity to
+        reflect where the player last moved.
+        """
+        super().update()
+
+        if self.x_speed > 0:
+            self.bullet_x_speed = self.bullet_speed
+        elif self.x_speed < 0:
+            self.bullet_x_speed = -self.bullet_speed
 
 
 class Zombie(BaseSprite):
